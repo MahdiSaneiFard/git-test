a592b1fea3801c9586793b9a61df9281df5259f0
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-16 18:16:17 +0000

    Tue, Jul 16, 2024, 9:46 PM +03:30

--- 2.c
+++ 2.c
@@ -3,5 +3,12 @@
 
 int main(void)
 {
-    printf("hello\n");
+    int first_number = get_int("write the first number :\n");
+    int secund_number = ger_int ("wirte the secound number :\n");
+
+    if (first_number > secund_number)
+    {
+        ptintf("%s is biger then %s", first_number , secund_number);
+    }
+    else if()
 }
