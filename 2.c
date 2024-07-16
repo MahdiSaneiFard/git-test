a0929e261dfef4d3058ba5c2677a4b00c0b6a3b9
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-16 18:22:22 +0000

    Tue, Jul 16, 2024, 9:52 PM +03:30

--- 2.c
+++ 2.c
@@ -4,11 +4,16 @@
 int main(void)
 {
     int first_number = get_int("write the first number :\n");
-    int secund_number = ger_int ("wirte the secound number :\n");
+    int secund_number = get_int ("wirte the secound number :\n");
 
     if (first_number > secund_number)
     {
-        ptintf("%s is biger then %s", first_number , secund_number);
+        ptintf("%d is biger then %d\n", first_number, secund_number);
     }
-    else if()
+    else if(first_number < secund_number)
+    {
+        printf("%d is biger then %d\n",secund_number,first_number);
+    }
+    else
+    printf("you enterd same numbers")
 }
