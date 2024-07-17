4211c84d8cd3391233310a5e129eb70818cfe9b4
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-17 15:32:14 +0000

    Wed, Jul 17, 2024, 7:02 PM +03:30

--- primed.c
+++ primed.c
@@ -4,13 +4,13 @@ int main(void)
 {
 for (int i = 0; i<5; i++)
 {
-    for (int j=0;j<5;j++)
+    for (int j=5;j>i;j--)
     {
         printf("#");
         while (i == j)
         {
             printf("\n");
-            i += 2;
+            j -= i;
         }
     }
 }
