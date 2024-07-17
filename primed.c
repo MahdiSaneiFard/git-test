8acda20bd1e8aff29417c3ed9bac6f998185354d
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-17 17:07:40 +0000

    Wed, Jul 17, 2024, 8:37 PM +03:30

--- primed.c
+++ primed.c
@@ -5,7 +5,7 @@ void f(int l,int p);
 int main(void)
 {
 int x = get_int("num : ");
-    for (int l=1, int d=x; l<x+1, d>-1; l++,d--)
+    for (int l=1, int d; l<x+1, d>-1; l++,d--)
     {
         f(l,d);
     }
