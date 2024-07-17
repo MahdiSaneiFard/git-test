a93b1056a1a37083e8f23c0cecf7e6f16bbc5d15
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-17 17:17:42 +0000

    Wed, Jul 17, 2024, 8:47 PM +03:30

--- primed.c
+++ primed.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <cs50.h>
 
-void f(int l,int p);
+void f(int l);
 int main(void)
 {
-int x = get_int("num : ");
+    int x = get_int("num : ");
     for (int l=1; l<x+1; l++)
     {
-        for (int d=x; d>-1; d--)
-        f(l,d);
+        f(l);
     }
+
 }
-void f(int l,int p)
+void f(int l)
 {
     for (int i=l; i > 0; i--)
     {
-
-        for (int i=p; i > 0; i--)
-        {
-            printf(" ");
-        }
         printf("#");
     }
     printf("\n");
