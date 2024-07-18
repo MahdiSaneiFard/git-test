47ea47997851ae6e73eb0ffc28f68f4ca9826ca6
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-18 16:21:10 +0000

    Thu, Jul 18, 2024, 7:51 PM +03:30

--- 3.c
+++ 3.c
@@ -8,7 +8,7 @@ int main(void)
 {
     for (int i = 0 ;i < leghte; i++)
     int score[i] = get_int("score :")
-    ptintf("%f", sum(leghte)  (float) leghte);
+    ptintf("%f", sum(int leghte)  (float) leghte);
 }
 int sum(int o)
 {
