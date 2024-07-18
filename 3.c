4f574d3398cebb7dd043a68cb36585bc5a47a060
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-18 16:20:25 +0000

    Thu, Jul 18, 2024, 7:50 PM +03:30

--- 3.c
+++ 3.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <cs50.h>
+
 int leghte = 3;
+
+int sum(int o);
 int main(void)
 {
     for (int i = 0 ;i < leghte; i++)
     int score[i] = get_int("score :")
-    ptintf("%f", sum(score[]) / (float) leghte);
+    ptintf("%f", sum(leghte)  (float) leghte);
 }
 int sum(int o)
 {
