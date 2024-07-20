9a4e5f9ba8c413854f8017a198fe0e5094c908fe
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-20 11:33:30 +0000

    Sat, Jul 20, 2024, 3:03 PM +03:30

--- averaage.c
+++ averaage.c
@@ -1,11 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
-
+int l = 11;
 int main(void)
 {
-    for(int i =0;i < 11; i++)
+int score[];
+    for(int i =0;i < l; i++)
     {
         int score[] = get_int("your score :");
     }
-    
+int g = sum(l , score[]);
+printf("%i", g / (float) l )
+}
+int sum(int vary,int scores[])
+{
+    int x=0;
+    for(int i = 0; i < l; i++)
+    x += socres[i]
 }
