8edf8aff5131948d2fa0b3b64651e2ddd34adc12
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-21 09:23:21 +0000

    Sun, Jul 21, 2024, 12:53 PM +03:30

--- array.c
+++ array.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
+#include <cs50.h>
+#include <string.h>
 
-
+int main(void)
+{
+    string name = get_string("your name :");
+    int x = strlen(name);
+    for(int i = 0; i <= x; i++)
+    {
+        if(i != x)
+        printf("%i\n", name[i]);
+        else
+        printf("nul\n");
+    }
+}
