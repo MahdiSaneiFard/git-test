86b2de5316b93ca438742f32752128deb89bb7a5
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-22 14:34:33 +0000

    Mon, Jul 22, 2024, 6:04 PM +03:30

--- tringel.c
+++ tringel.c
@@ -12,21 +12,21 @@ int main(void)
     {
         int g = sum(i);
         finn(g,i);
-        printf("\n");
 
     }
 }
 int sum(int l)
 {
     int sum = 0;
-        for(int u = 0; u < l; u++)
+    for(int u = 0; u < l--; u++)
     sum = sum + array[u];
     return sum;
-
+}
 void finn(int g, int l)
 {
     for(int o = g; o++<=g+l; o++)
     {
         printf("%i", g);
     }
+    printf("\n");
 }
