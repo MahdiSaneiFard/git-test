ac27314551eaa16d61b042faf185e15081a6ec40
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-22 12:58:01 +0000

    Mon, Jul 22, 2024, 4:28 PM +03:30

--- tringel.c
+++ tringel.c
@@ -2,16 +2,20 @@
 #include <cs50.h>
 
 int leghte = 5;
+
+void f(int l);
 int main(void)
 {
     for(int i=1; i<=leghte; i++)
     {
-
+        f(leghte);
     }
 }
-string f(int l)
+void f(int l)
 {
     for(int i = 1; i<l; i++)
-    int array[i];
-    printf("%d" )
+    {
+        int array[i];
+        printf("%d" array[i--] );
+    }
 }
