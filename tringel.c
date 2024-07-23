1c76bf67fa8801fc9af391e1d5f1f410496b9c4a
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-23 10:25:29 +0000

    Tue, Jul 23, 2024, 1:55 PM +03:30

--- tringel.c
+++ tringel.c
@@ -4,35 +4,30 @@
 int sum(int l,int array[]);
 void finn(int g, int l);
 void space(int i, int num);
-int main(void)
-{
+int main(void){
     int num = get_int("num ");
     int array[num];
     for(int l = 0; l < num; l++)
         array[l] = l+1 ;
-    for(int i=1; i<=num; i++)
-    {
+    for(int i=1; i<=num; i++){
         space(i,num);
         int g = sum(i, array);
         finn(g+1,i);
 
     }
 }
-int sum(int l,int array[])
-{
+int sum(int l,int array[]){
     int sum = 0;
     for(int u = 0; u < l; u++)
     sum = sum + array[u]-1;
     return sum;
 }
-void finn(int g, int l)
-{
+void finn(int g, int l){
     for(int o = g; o < g+l; o++)
     printf("%i ", o);
     printf("\n");
 }
-void space(int i, int num)
-{
+void space(int i, int num){
     for(int l = 1;l < num - i; l++)
     printf(" ");
 }
