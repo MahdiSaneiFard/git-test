911d0e90b5029d0881b6f22e4bfcbccb2dbebae1
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-25 13:21:28 +0000

    Thu, Jul 25, 2024, 4:51 PM +03:30

--- pattern.c
+++ pattern.c
@@ -3,6 +3,7 @@
 
 const int vary = 3;
 
+bool check_working(int numbers[]);
 bool check_geometry(int numbers[]);
 int main(void)
 {
@@ -14,15 +15,24 @@ int main(void)
     }
     bool check_g = check_geometry(numbers);
     if(check_g == true)
-    printf("! هندسی است\n");
+    printf("! هندسی است your d is %i \n", numbers[2] / numbers[1]);
     bool check_h = check_working(numbers);
-
-
+    if(check_g == true)
+    printf("! حسابی است your d is %i \n", numbers[2] - numbers[1]);
 }
 bool check_geometry(int numbers[])
 {
     int l = numbers[1] / numbers[0];
     int g = numbers[2] / numbers[1];
+    if(g==l)
+    return true;
+    else
+    return false;
+}
+bool check_working(int numbers[])
+{
+    int l = numbers[1] - numbers[0];
+    int g = numbers[2] - numbers[1];
     if(g==l)
     return true;
     else
