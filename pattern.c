e15688354833132424ae4ca343fc5bb76c4455b1
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-25 13:44:29 +0000

    Thu, Jul 25, 2024, 5:14 PM +03:30

--- pattern.c
+++ pattern.c
@@ -4,6 +4,7 @@ const int vary = 3;
 
 bool check_working(int numbers[]);
 bool check_geometry(int numbers[]);
+int check_finn(int numbers[]);
 int main(void)
 {
     int numbers[vary];
@@ -19,6 +20,11 @@ int main(void)
     bool check_h = check_working(numbers);
     if(check_g == true)
     printf("! حسابی است your d is %i \n", numbers[2] - numbers[1]);
+    else
+    {
+        int finn = check_finn(numbers);
+        printf("your next num is %i\n", finn);
+    }
 }
 
 bool check_geometry(int numbers[])
@@ -39,4 +45,10 @@ bool check_working(int numbers[])
     return true;
     else
     return false;
+}
+int check_finn(int numbers[])
+{
+    int p1 = numbers[1] - numbers[0];
+    int p2 = numbers[2] - numbers[1];
+    return p2 - p1 + p2 + numbers[2];
 }
