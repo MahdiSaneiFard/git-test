3dc603c3dba02c51b22b4035e058b6e7cd61c3dd
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-07-25 12:46:03 +0000

    Thu, Jul 25, 2024, 4:16 PM +03:30

--- pattern.c
+++ pattern.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <cs50.h>
 
 const int vary = 3;
 
+bool check_geometry(int numbers[]);
 int main(void)
 {
     int numbers[vary];
@@ -10,10 +12,16 @@ int main(void)
         printf("your num\n");
         scanf("%i",  &numbers[c]);
     }
+    check_geometry(numbers);
 
 
 }
-bool check_ghometry(numbers[c])
+bool check_geometry(int numbers[])
 {
-    
+    numbers[1] \ numbers[0] = int l;
+    numbers[2] \ numbers[1] = int g;
+    if(g==l)
+    return 0;
+    else
+    return 1;
 }
