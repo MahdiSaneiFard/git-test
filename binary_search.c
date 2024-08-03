19229b2b9fabd02760af822b7e6ee42ac8143727
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-08-03 09:29:13 +0000

    Sat, Aug 3, 2024, 12:59 PM +03:30

--- binary_search.c
+++ binary_search.c
@@ -5,21 +5,26 @@
 int data[]={1,2,3,4,5,6,7,8,9,10,11};
 int r = 0;
 
-void binary_search(int up,int d,int lp);
+bool binary_search(int up,int d,int lp);
 int main(void)
 {
     int d = (sizeof(data)/4) - 1;
     printf("what you want to search ");
     int user_input;
     scanf("%i", &user_input);
-    binary_search(user_input,d,d);
+    bool answer = binary_search(user_input,d,d);
+    if (answer == 0)
+    printf ("found\n");
+    else
+    printf("not found");
+
 }
 
-void binary_search(int up,int d,int lp)
+bool binary_search(int up,int d,int lp)
 {
     if(data[d/2] == up)
     {
-        printf("found \n");
+        return 0;
     }
     else if(data[d/2] > up)
     {
