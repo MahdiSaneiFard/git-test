cef9315565495219be1c97e048ddcb95bbe6bcab
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-08-03 07:15:25 +0000

    Sat, Aug 3, 2024, 10:45 AM +03:30

--- binary_search.c
+++ binary_search.c
@@ -1,12 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <str>
+#include <string.h>
 
 int data[]={1,3,5,7,9};
 
 void binary_search(int l);
 int main(void)
 {
+    int d = strlen(data);
     printf("what you want to search ");
     int user_input;
     scanf("%i", &user_input);
