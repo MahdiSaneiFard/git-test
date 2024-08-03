6ea5f4f0289cc556632420b49eebdb0f320280e0
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-08-03 08:44:36 +0000

    Sat, Aug 3, 2024, 12:14 PM +03:30

--- binary_search.c
+++ binary_search.c
@@ -8,7 +8,7 @@ int r = 0;
 void binary_search(int up,int d);
 int main(void)
 {
-    int d = sizeof(data)/4;
+    int d = (sizeof(data)/4) -1;
     printf("what you want to search ");
     int user_input;
     scanf("%i", &user_input);
