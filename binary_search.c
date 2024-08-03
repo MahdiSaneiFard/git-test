3393c2f43fc865030cf6808a84cfae668e588afe
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-08-03 17:23:19 +0000

    Sat, Aug 3, 2024, 8:53 PM +03:30

--- binary_search.c
+++ binary_search.c
@@ -5,6 +5,34 @@
 int data[]={1, 3, 5, 7, 9, 11};
 int r = 0;
 
+int binary_search_right(int up,int d,int lp);
+int binary_search_left(int up,int d,int lp);
+int main(void)
+{
+    int d = (sizeof(data)/4) - 1;
+    printf("what you want to search ");
+    int user_input;
+    scanf("%i", &user_input);
+    if(user_input<=data[d/2])
+    {
+        int a = binary_search_left(user_input,d,d);
+        if(a == 0)
+        printf("found\n");
+        else
+        printf("not found \n");
+        return 0;
+    }
+    if(user_input>data[d/2])
+    {
+        int a = binary_search_right(user_input,d/2,d);
+        if(a == 0)
+        printf("found\n");
+        else
+        printf("not found \n");
+        return 0;
+    }
+}
+
 int binary_search_left(int up,int d,int lp)
 {
     while (r <=lp/2 +1)
@@ -31,6 +59,7 @@ int binary_search_left(int up,int d,int lp)
     return 1;
 
 }
+
 int binary_search_right(int up,int d,int lp)
 {
     while (r <=lp/2 +1)
@@ -56,33 +85,6 @@ int binary_search_right(int up,int d,int lp)
     }
     return 1;
 
-}
-
-int main(void)
-{
-    int d = (sizeof(data)/4) - 1;
-    printf("what you want to search ");
-    int user_input;
-    scanf("%i", &user_input);
-    if(user_input<=data[d/2])
-    {
-        int a = binary_search_left(user_input,d,d);
-        if(a == 0)
-        printf("found\n");
-        else
-        printf("not found \n");
-        return 0;
-    }
-    if(user_input>data[d/2])
-    {
-        int a = binary_search_right(user_input,d/2,d);
-        if(a == 0)
-        printf("found\n");
-        else
-        printf("not found \n");
-        return 0;
-    }
-    }
 }
 
 
