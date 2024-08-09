fdc224dcf8825e9fb6046e054df2c30387a492da
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-08-09 07:45:44 +0000

    Fri, Aug 9, 2024, 11:15 AM +03:30

---
+++ fibonachi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int user_input;
+    printf("how many number do you need \n:");
+    scanf("%i", &user_input);
+    fibonach(user_input);
+}
+
+int fibonachi(int i)
+{
+    int n[i];
+    n[0] == 0;
+    n[1] == 1;
+    n
+}
