f9d6043cdc5d95d40e70525eaf9512e37c9d7089
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-08-09 07:49:23 +0000

    Fri, Aug 9, 2024, 11:19 AM +03:30

--- fibonachi.c
+++ fibonachi.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
+int fibonachi(int i);
 int main(void)
 {
     int user_input;
     printf("how many number do you need \n:");
     scanf("%i", &user_input);
-    fibonach(user_input);
+    printf("%i", fibonachi(user_input));
 }
 
 int fibonachi(int i)
 {
     int n[i];
-    n[0] == 0;
-    n[1] == 1;
-    n
+    n[0] = 0;
+    n[1] = 1;
+    n[i] = n[i--] + n[i-2];
+    return n[i];
 }
