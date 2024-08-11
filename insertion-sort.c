7910423c84b1a9c4a6db01e7baa0d6d652c186fc
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-08-11 07:00:23 +0000

    Sun, Aug 11, 2024, 10:30 AM +03:30

--- insertion-sort.c
+++ insertion-sort.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-string A[] = {3, 2, 5, 4, 1};
+int A[] = {3, 2, 5, 4, 1};
 
 int main(void)
 {
     int size = sizeof(A)/4;
-    for(int i = 2; i <= size ; i++)
+    for(int i = 1; i <= size ; i++)
     {
         int key = A[i];
-        for(int j = i - 1; j > 0 && A[i] < A[j]; j--)
+        for(int j = i - 1; j >= 0 && A[i] < A[j]; j--)
         A[j + 1] = A[j];
 
     }
-    printf("%i", A);
+    for(int i = 0; i < size; i++)
+    printf("%i", A[i]);
+    printf("\n");
 }
