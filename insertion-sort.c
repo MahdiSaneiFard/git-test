e308e8190f66d6bfd8a8e2cbf2680e192c9b1a1e
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-08-11 06:53:40 +0000

    Sun, Aug 11, 2024, 10:23 AM +03:30

--- insertion-sort.c
+++ insertion-sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-A[] = {3, 2, 5, 4, 1};
+int A[] = {3, 2, 5, 4, 1};
 
 int main(void)
 {
@@ -9,6 +9,8 @@ int main(void)
     for(int i = 2; i <= size ; i++)
     {
         int key = A[i];
-        for(int)
+        for(int j = i - 1; j > 0 && A[i] < A[j]; j--)
+        A
+
     }
 }
