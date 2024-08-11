7a90be53b28e9c8967b8ddf6a96f691a5e04804c
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-08-11 07:12:49 +0000

    Sun, Aug 11, 2024, 10:42 AM +03:30

--- insertion-sort.c
+++ insertion-sort.c
@@ -12,9 +12,14 @@ int main(void)
         int j;
         for(j = i - 1; j >= 0 && A[i] < A[j]; j--)
         A[j + 1] = A[j];
+
+        for(int il = 0; il < size; il++)
+        printf("%i", A[il]);
+        printf("\n");
+
         A[j] = key;
     }
-    for(int i = 0; i < size; i++)
-    printf("%i", A[i]);
+    for(int il = 0; il < size; il++)
+    printf("%i", A[il]);
     printf("\n");
 }
