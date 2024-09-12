7e60e0598904519ab65300ec9f6cafac2e28cd4a
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-09-12 12:35:30 +0000

    Thu, Sep 12, 2024, 4:05 PM +03:30

--- memory-test.c
+++ memory-test.c
@@ -3,15 +3,11 @@
 int main(void)
 {
     printf("ready\n");
-    int s = 1;
-    int* p = &s;
-    printf("%p\n", &s);
-    printf("%d\n", *(p+5));
     char* d = "a";
-    for(int u = 1, u < 4, u++)
+    for(int u = 0; u < 10000; u++)
     {
         char* l = (d+u);
-        printf("%c\n", *(d+u));
+        printf("%i", *(d+u));
 
     }
 
