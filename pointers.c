823df3be5af4f9d0c64c0eae6c0fc20230977bc6
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-09-23 17:19:26 +0000

    Mon, Sep 23, 2024, 8:49 PM +03:30

--- pointers.c
+++ pointers.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int swap_y(t,y)
+int swap_y(int t,int y)
+{
+    y = t;
+    return y;
+}
 
 int swap(int x, int y)
 {
     int t = x;
     x = y;
-    swap_y(t,y);
     return x;
 }
 
@@ -22,8 +25,9 @@ int main(void)
 
     printf("x: %i & y: %i\n", x, y);
 
+    int t = x;
     swap(x, y);
-    swap(x, y);
+    swap_y(t, y);
 
     printf("x: %i & y: %i", x, y);
 }
