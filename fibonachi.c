6f2cf2ff4dd353a2610fdbde79274974afe47714
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-01 19:16:43 +0000

    Tue, Oct 1, 2024, 10:46 PM +03:30

--- fibonachi.c
+++ fibonachi.c
@@ -11,24 +11,10 @@ int main(void)
     int a[user_input];
     int *p = malloc(4 * user_input);
     printf("%i\n", fibonachi(user_input,p));
-    main2();
 }
 
 int fibonachi(int i, int *a)
 {
-    *(a) = 1;
-    *(a+4) = 1;
-    *(a+(i*4)-4) = *(a+(i*4)-8)+ *(a+(i*4)-12);
-    i--;
-    return *(a+(i*4)-4);
+
 }
 
-int main2(void)
-{
-    int l = 8;
-    int y[l];
-    y[0]=1;
-    y[1]=1;
-    y[l-1] = y[l-2] + y[l-3];
-    return y[l-1];
-}
