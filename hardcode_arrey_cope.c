fcf1e98d1d7b1396ed9f4a170d8198d6975ec615
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-01 17:25:54 +0000

    Tue, Oct 1, 2024, 8:55 PM +03:30

--- hardcode_arrey_cope.c
+++ hardcode_arrey_cope.c
@@ -1,2 +1,16 @@
-include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argv, char *argc)
+{
+    int arrey[3];
+    arrey[0] = 1;
+    arrey[1] = 2;
+    arrey[2] = 3;
+
+    for(int i = 0; i < 3; i =+ 1)
+    {
+        printf("%i is your number", arrey[i]);
+    }
+    printf("\n");
+}
