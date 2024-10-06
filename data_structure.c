e2a93ff9ca6e5dc3d3ade03e640dd9606b106043
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-06 02:26:03 +0000

    Sun, Oct 6, 2024, 5:56 AM +03:30

--- data_structure.c
+++ data_structure.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct node
+{
+    int Number;
+    struct node *Next;
+} nude;
 
 int main(void)
 {
     printf("enter the number :");
-    char *group = malloc(sizeof(int)) ;
+    char *group = malloc(4 * sizeof(int));
     scanf("%s", group);
-    printf("%s", group);
+    int *list = NULL;
+    for(int i = strlen(), int *n; i > 0; i++)
+    {
+        n = group[i];
+        list ->
+    }
+
 }
