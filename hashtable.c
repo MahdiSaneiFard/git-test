b5ebbac5ebb547c8d07ce2a61f09c89ec1e7dddf
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-07 10:26:18 +0000

    Mon, Oct 7, 2024, 1:56 PM +03:30

--- hashtable.c
+++ hashtable.c
@@ -24,17 +24,10 @@ int main(int count ,char *name[])
     //array get the values as program data
     for(int k =0; k < 26; k++)
     ab[k].ST=k+1;
-
-    for(int i = 1; i <= 26; i++)
-    {
-
-        node *n = malloc(sizeof(node));
-        n->Name = name[i];
-        ab[i].p = n;
-    }
+    
     for(int i = 0; i <= 26; i++)
     {
-        node *c[i];
+        node *c[i] =NULL;
 
         int l = 1;
         do{
@@ -43,7 +36,8 @@ int main(int count ,char *name[])
             {
                 node *n = malloc(sizeof(node));
                 n->Name = name[l];
-                
+                n->Next = c[i];
+
 
             }
 
