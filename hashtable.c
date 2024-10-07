d9275cdddee146f28b3d941c2d9001731d99be05
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-07 06:09:32 +0000

    Mon, Oct 7, 2024, 9:39 AM +03:30

--- hashtable.c
+++ hashtable.c
@@ -25,12 +25,13 @@ int main(int count ,char *name[])
     //array get the values as program data
     for(int k =0; k < 26; k++)
     ab->ST[k]=k+1;
+    for(int i; i < count; i++)
 
     for(int i = 1; i < count; i++)
     {
         //check_first_letter
         int f = toupper(name[i][0]) - 'A';
-        name[f]
+        ab[i]->p = name [i]
 
     }
 }
