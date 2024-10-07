151d1676b5c682364b7494828b3397f00486230e
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-07 04:53:51 +0000

    Mon, Oct 7, 2024, 8:23 AM +03:30

--- hashtable.c
+++ hashtable.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <strlab.h>
+#include <strlib.h>
 
 //node
 typedef struct node
 {
-    
+    char *Name;
+    struct node *Next;
 }
 
 int main(int count ,char *name[])
