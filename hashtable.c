58ac43c8efd6472fc2ede321bfef224eaccc0ec8
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-07 06:15:11 +0000

    Mon, Oct 7, 2024, 9:45 AM +03:30

--- hashtable.c
+++ hashtable.c
@@ -2,30 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
 //array
+
 typedef struct arr
 {
     int ST;
     struct node *p;
 }arr;
+
 //node
 typedef struct node
 {
     char *Name;
     struct node *Next;
 }node;
+
 int main(int count ,char *name[])
 {
     arr ab[26];
+
     //array get the values as program data
     for(int k =0; k < 26; k++)
-    ab[k]->ST = k+1;
-    for(int i; i < count; i++)
+    ab->ST=k+1;
 
     for(int i = 1; i < count; i++)
     {
         //check_first_letter
         int f = toupper(name[i][0]) - 'A';
         ab[i]->p = name [i]
+
     }
 }
