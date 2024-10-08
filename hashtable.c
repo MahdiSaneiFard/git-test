2aab24ece94fe82096ae5d7e9c5b6314c3294964
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-08 08:43:43 +0000

    Tue, Oct 8, 2024, 12:13 PM +03:30

--- hashtable.c
+++ hashtable.c
@@ -51,6 +51,7 @@ int main(int count ,char *name[])
     node *ptr = ab[a].p;
         while (ptr != NULL)
         {
+            printf("%c: ", ab[a].ST+64);
             printf("%s ", ptr->Name);
             ptr = ptr->Next;
         }
