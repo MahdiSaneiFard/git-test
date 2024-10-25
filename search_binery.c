570dcd4789212efefae29c7ecc6a93dda8c1c3ee
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-25 12:55:24 +0000

    Fri, Oct 25, 2024, 4:25 PM +03:30

--- search_binery.c
+++ search_binery.c
@@ -4,7 +4,7 @@ int main(int countity, char *argev[])
 {
     printf("what you want to search here : ");
     char *target;
-    scanf("%d", &target)
+    scanf("%s", &target)
     for(int i = 0; i < countity; i += 1)
     {
 
