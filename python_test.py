41893eb02f5f2d17e8ad4982fd1dae8079a3e6e8
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-28 08:05:10 +0000

    Mon, Oct 28, 2024, 11:35 AM +03:30

--- python_test.py
+++ python_test.py
@@ -2,9 +2,20 @@ from qrcode import
 
 def get_Link():
     link = import("enter your link : ")
+    return link
 
 def creating_qrcode(link):
     img = qrcode.make(f"{link}")
     img.save("qrcode_image.png", "PNG")
-    
+
+def open_pic()
+    print(open("qrcode_image.png").read())
+
+def main()
+    link = get_Link()
+    creating_qrcode(link)
+    open_pic()
+
+main()
+
 
