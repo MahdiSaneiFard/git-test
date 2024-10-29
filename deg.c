5d71002f2370fbce2bde0cdd7101d2e067b9a105
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-29 18:07:59 +0000

    Tue, Oct 29, 2024, 9:37 PM +03:30

--- deg.c
+++ deg.c
@@ -2,5 +2,10 @@
 
 int main(void)
 {
-    printf("")
+    int x[3];
+    printf("enter the point : \n");
+    for(int i = 0; i < 3; i++)
+        x[i] = scanf("");
+    if(x[0]>0 && x[1]>0 && x[2]>0)
+    printf("in first ");
 }
