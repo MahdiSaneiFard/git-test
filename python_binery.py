5cd3132d5b204be36754f4b06408f8668b1a6080
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-30 19:17:19 +0000

    Wed, Oct 30, 2024, 10:47 PM +03:30

---
+++ python_binery.py
@@ -0,0 +1,4 @@
+first_num = input("enter your first number ")
+secound_num = input("enter your secound number ")
+print("what operation you want to do with your numbers in binery")
+operation = input("sum : 's' , mines : 'm' ")
