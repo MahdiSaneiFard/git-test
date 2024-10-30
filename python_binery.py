3f8b8c4739a4fff3dcf03c737ce92971574f7c5f
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-10-30 19:37:06 +0000

    Wed, Oct 30, 2024, 11:07 PM +03:30

--- python_binery.py
+++ python_binery.py
@@ -1,8 +1,12 @@
 first_num = input("enter your first number ")
 secound_num = input("enter your secound number ")
+if ((first_num or secound_num) is not(int)):
+    print('error')
 print("what operation you want to do with your numbers in binery")
 operation = input("sum : 's' , mines : 'm' ")
-while operation is not("m" or 's') and:
+while operation is not("m" or 's'):
     operation = input("sum : 's' , mines : 'm' ")
-print()
+print(bin(first_num))
+print(bin(secound_num))
+print(bin(first_num + secound_num))
 
