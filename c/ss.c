44bbf91d61ab63bf693691b022f626a3a4db066a
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-11-02 17:56:43 +0000

    Sat, Nov 2, 2024, 9:26 PM +03:30

--- c/ss.c
+++ c/ss.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
  int main()
  {
-    int a = 0x8000 , x = -127 , y = 10;
+ a = 0x8000 , x = -127 , y = 10;
     int b = 0x7ff2;
     short w = 0x8000 , u = 0x7ff2;
     int z = x+y;
-    printf("a = %p",&a);
+    printf("a = %d",*a);
 
  }
