2a7f74b66a3461bb3d7579915a149bf7812461d3
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-11-05 17:14:36 +0000

    Tue, Nov 5, 2024, 8:44 PM +03:30

--- c/fibonachi.c
+++ c/fibonachi.c
@@ -6,7 +6,7 @@ int main(void)
 {
     int user_input;
     printf("how many number do you need \n:");
-    scanf("%i", &user_input);
+    scanf("%d", &user_input);
     int al[user_input];
     int *p = malloc(4 * user_input);
     printf("%i\n", fibonachi(user_input));
