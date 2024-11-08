2d4ba27e44ccd29b3f62a1e687b6f6fe5d91fcee
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-11-08 18:04:25 +0000

    Fri, Nov 8, 2024, 9:34 PM +03:30

--- hoss.c
+++ hoss.c
@@ -5,7 +5,7 @@ int main()
     int sum = 0;
     int l;
     int o;
-    printf("how many numbers do you want to enter?\n");
+    printf("how many numbers do you want to know the averge of them ?\n");
     scanf("%d", &l);
     for(int i = 0; i < l; i++)
     {
@@ -13,4 +13,5 @@ int main()
         scanf("%d", &o);
         sum = o + sum;
     }
+    printf("your number is %d", sum/l);
 }
