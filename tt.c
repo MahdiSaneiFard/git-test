11eb0e722a90775524826e466fd315761bf0d293
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-11-09 21:52:47 +0000

    Sun, Nov 10, 2024, 1:22 AM +03:30

--- tt.c
+++ tt.c
@@ -8,9 +8,48 @@ int main(void)
     printf("by orther :");
     char ca , cb, cc;
     scanf("%c %c %c", &ca, &cb ,&cc);
-
-    if(a < b)
+    int temp;
+    if(a > b)
     {
-        
+        temp = a;
+        a =b;
+        b =temp;
     }
+    if(c<b)
+    {
+        if(c<a)
+        {
+            temp = a;
+            a =c;
+            c=temp;
+            temp =c;
+            c = b;
+            b =temp;
+        }
+    }
+    int A = a;
+    int B = b;
+    int C = c;
+    if(ca == 'A')
+    {
+        if(cb == 'B')
+        printf("%d %d %d\n",A ,B ,C);
+        if(cc == 'B')
+        printf("%d %d %d\n" , A ,C ,B);
+    }
+    if(ca == 'B')
+    {
+        if(cb == 'A')
+        printf("%d %d %d\n",B ,A ,C);
+        if(cc == 'A')
+        printf("%d %d %d\n" , B ,C ,A);
+    }
+    if(ca == 'C')
+    {
+        if(cb == 'B')
+        printf("%d %d %d\n",C ,B ,A);
+        if(cc == 'B')
+        printf("%d %d %d\n" , C,A ,B);
+    }
+
 }
