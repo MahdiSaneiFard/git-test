7514c2965e9d0041c9bf8a1e4ca01a3645742a2c
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-11-09 21:40:52 +0000

    Sun, Nov 10, 2024, 1:10 AM +03:30

--- t.c
+++ t.c
@@ -5,30 +5,29 @@ int main(void)
     int a, b ,c;
     printf("nums :");
     scanf("%d %d %d ", &a, &b ,&c);
-
-    char ca , cb, cc;
     printf("by orther");
+    char ca , cb, cc;
     scanf("%c %c %c", &ca, &cb ,&cc);
     if(ca == 'A')
     {
         if(cb == 'B')
-        printf("%d %d %d",a ,b ,c);
+        printf("%d %d %d\n",a ,b ,c);
         if(cc == 'B')
-        printf("%d %d %d" , a ,c ,b);
+        printf("%d %d %d\n" , a ,c ,b);
     }
     if(ca == 'B')
     {
         if(cb == 'A')
-        printf("%d %d %d",b ,a ,c);
+        printf("%d %d %d\n",b ,a ,c);
         if(cc == 'A')
-        printf("%d %d %d" , b ,c ,a);
+        printf("%d %d %d\n" , b ,c ,a);
     }
     if(ca == 'C')
     {
         if(cb == 'B')
-        printf("%d %d %d",c ,b ,a);
+        printf("%d %d %d\n",c ,b ,a);
         if(cc == 'B')
-        printf("%d %d %d" , c ,a ,b);
+        printf("%d %d %d\n" , c ,a ,b);
     }
 
 
