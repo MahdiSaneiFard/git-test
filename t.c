3a257e1d6249b9a30853ecc6563161eb1bdc0921
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-11-09 21:42:09 +0000

    Sun, Nov 10, 2024, 1:12 AM +03:30

--- t.c
+++ t.c
@@ -5,7 +5,7 @@ int main(void)
     int a, b ,c;
     printf("nums :");
     scanf("%d %d %d ", &a, &b ,&c);
-    printf("by orther");
+    printf("by orther :");
     char ca , cb, cc;
     scanf("%c %c %c", &ca, &cb ,&cc);
     if(ca == 'A')
