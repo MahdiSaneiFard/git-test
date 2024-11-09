025a371f7cee85c66114d52d5a6244f2637a15d1
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-11-09 21:32:46 +0000

    Sun, Nov 10, 2024, 1:02 AM +03:30

--- t.c
+++ t.c
@@ -12,8 +12,24 @@ int main(void)
     if(ca == 'A')
     {
         if(cb == 'B')
-        printf("%d %d %d",a ,b ,c)
+        printf("%d %d %d",a ,b ,c);
         if(cc == 'B')
+        printf("%d %d %d" , a ,c ,b);
     }
+    if(ca == 'B')
+    {
+        if(cb == 'A')
+        printf("%d %d %d",b ,a ,c);
+        if(cc == 'A')
+        printf("%d %d %d" , b ,c ,a);
+    }
+    if(ca == 'C')
+    {
+        if(cb == 'B')
+        printf("%d %d %d",a ,b ,c);
+        if(cc == 'B')
+        printf("%d %d %d" , a ,c ,b);
+    }
+
 
 }
