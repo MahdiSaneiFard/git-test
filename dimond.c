04063ae6d0ba75bde2080d35adf8f7fcf179bd3f
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-11-11 21:41:50 +0000

    Tue, Nov 12, 2024, 1:11 AM +03:30

--- dimond.c
+++ dimond.c
@@ -17,7 +17,7 @@ int main(void)
         {
             printf("*");
         }
-        for(int ll = num-i;ll > 0; ll-=2)
+        for(int ll = num-i;ll > 0; ll-=1)
         printf(" ");
         for(int k = 1; k < i; k++)
         printf("*");
