89bdbad582de9e6f859d9ee393bbd113beab64e8
Mahdi01001101 <mahdi.sanei8413@gmail.com>
2024-11-30 18:20:40 +0000

    Sat, Nov 30, 2024, 9:50 PM +03:30

--- mmm.c
+++ mmm.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
 
+;
 int main(void)
 {
+    int b;
+    int max;
+    int min;
+    int a;
 
-    scanf("%d", &a);
-    
+    do{
+        scanf("%d%d", &a,&b);
+    }while(a<=0 || b<=0);
+
+    if(a < b)
+    {
+        min = a;
+        max = b;
+    }
+    else
+    {
+        min = b;
+        max = a;
+    }
+    for(int i = min; i >= 1; i--)
+    {
+        if(max%i == 0)
+        {
+            if(min%i == 0)
+            {
+                printf("%d",i);
+                break;
+            }
+        }
+
+    }
 }
-
